586780044e8812f0466fc4b108fe1f8ccace7106
Asaduz Zaman <asadiceiu@gmail.com>
2020-09-04 02:25:12 +1000

    Daily commit 4

--- data-finalize.py
+++ data-finalize.py
@@ -11,16 +11,18 @@ include more than one keywords.
 import pandas as pd, os, re
 import numpy as np, glob
 
-## Gather all downloaded data
 
-files = np.concatenate((glob.glob('grabbed_data/*.csv', recursive=True), glob.glob('grabbed_data/keyword/*.csv', recursive=True)))
-
+#files = np.concatenate((glob.glob('grabbed_data/*.csv', recursive=True), glob.glob('grabbed_data/keyword/*.csv', recursive=True)))
+# get all files fromgrabbed_data/keyword directory
+files = glob.glob('grabbed_data/keyword/*.csv', recursive=True)
+# read the first file
 df = pd.read_csv(files[0])
 count = len(df)
+# read all file and concat the dataset to the first dataset sequentially with dropping any duplicate entries based on the url
 for f in files:
     df2 = pd.read_csv(f)
     count += len(df2)
     df = pd.concat([df, df2]).drop_duplicates(subset=['url']).reset_index().drop(columns=['index','Unnamed: 0'])
     print(f, len(df), count)
-
+# write all data to a single csv file
 df.to_csv('reddit-all.csv')
--- reddit_scrapper.py
+++ reddit_scrapper.py
@@ -24,6 +24,42 @@ def preprocess_text(txt):
     txt = txt.replace("\n"," ").replace("\r"," ")
     return txt.encode('ascii', 'ignore').decode('ascii') ## converts all non-ascii characters to ascii
 
+
+keywords = ['vulnerability','cybersecurity', 'cyber-crime', 'cybercrime', 'cyber crime', 'CVE', 'CVEs', 'CVE-', 'cyber attack']
+
+for q in keywords:
+    print('\nKeyword: ', q)
+    url = 'https://api.pushshift.io/reddit/search/submission/?q={}&after=2071d&aggs=subreddit&size=500'.format(q)
+    subs_r = requests.get(url)
+    subs = pd.DataFrame(subs_r.json()["aggs"]['subreddit'])
+    for sub in subs['key']:
+        print("\nSUBREDDIT:",sub)
+        data_dict = {"topic":[],"subreddit":[],"title":[],"score":[],"id":[],"url":[],"created":[],"body":[]}
+        for d in range(1,2071,30):
+            try:
+                url_submission = "https://api.pushshift.io/reddit/search/submission/?q={}&subreddit={}&after={}d&before={}d&size=500".format(q,sub,d+30,d)
+                response = requests.get(url_submission)
+                json_data = response.json()["data"]
+                for submission in json_data:
+                    print(submission['author'],end=' ')
+                    txt = preprocess_text(submission['selftext'])
+                    if len(txt)<20: continue
+                    data_dict['topic'].append(q)
+                    data_dict['subreddit'].append(sub)
+                    data_dict['title'].append(preprocess_text(submission['title']))
+                    data_dict['score'].append(submission['score'])
+                    data_dict['id'].append(submission['id'])
+                    data_dict['url'].append(submission['url'])
+                    data_dict['created'].append(get_date(submission['created_utc']))
+                    data_dict['body'].append(txt)
+            except:
+                continue
+
+        sub_data = pd.DataFrame(data_dict)
+        sub_data.to_csv("grabbed_data/keyword/"+sub+"-data-"+q.replace(" ",'_')+".csv")
+
+
+'''
 #### Read Json From Pushshift API
 ## Getting data from 1 January 2015 onward
 #https://api.pushshift.io/reddit/search/submission/?q=cybersecurity&after=1500d&before=1250d&aggs=subreddit
@@ -70,42 +106,6 @@ for sub in np.array(combined['key'])[1:len(combined)]:
     sub_data = pd.DataFrame(data_dict)
     sub_data.to_csv("grabbed_data/"+sub+"-data-all.csv")
 
-keywords = ['cyber-crime', 'cybercrime', 'cyber crime', 'CVE', 'CVEs', 'CVE-', 'cyber attack']
-
-for q in keywords:
-    print('\nKeyword: ', q)
-    url = 'https://api.pushshift.io/reddit/search/submission/?q={}&after=2071d&aggs=subreddit&size=500'.format(q)
-    subs_r = requests.get(url)
-    subs = pd.DataFrame(subs_r.json()["aggs"]['subreddit'])
-    for sub in subs['key']:
-        print("\nSUBREDDIT:",sub)
-        data_dict = {"topic":[],"subreddit":[],"title":[],"score":[],"id":[],"url":[],"created":[],"body":[]}
-        for d in range(1,2071,30):
-            try:
-                url_submission = "https://api.pushshift.io/reddit/search/submission/?q={}&subreddit={}&after={}d&before={}d&size=500".format(q,sub,d+30,d)
-                response = requests.get(url_submission)
-                json_data = response.json()["data"]
-                for submission in json_data:
-                    print(submission['author'],end=' ')
-                    txt = preprocess_text(submission['selftext'])
-                    if len(txt)<20: continue
-                    data_dict['topic'].append(q)
-                    data_dict['subreddit'].append(sub)
-                    data_dict['title'].append(preprocess_text(submission['title']))
-                    data_dict['score'].append(submission['score'])
-                    data_dict['id'].append(submission['id'])
-                    data_dict['url'].append(submission['url'])
-                    data_dict['created'].append(get_date(submission['created_utc']))
-                    data_dict['body'].append(txt)
-            except:
-                continue
-
-        sub_data = pd.DataFrame(data_dict)
-        sub_data.to_csv("grabbed_data/keyword/"+sub+"-data-"+q.replace(" ",'_')+".csv")
-
-
-
-
-
+'''
 
 
